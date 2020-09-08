b6186d96821edb2706dd29fde18b63e416fe46d6
Lachlan Barclay <lachlan.barclay@mercer.com>
2020-09-08 14:33:18 +1000

    initial commit

---
+++ perf/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace perf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                CountryLogic countryLogic = new CountryLogic();
+
+                Stopwatch sw = Stopwatch.StartNew();
+
+                countryLogic.WritePeopleOrderedByCountry("output.txt");
+
+                sw.Stop();
+
+                Console.WriteLine("Application took: " + ((decimal)sw.ElapsedMilliseconds / 1000).ToString("N2") + " seconds");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                
+            }
+            Console.WriteLine("Press enter to quit...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ perf/classes/CountryLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace perf
+{
+    public class CountryLogic
+    {
+        private readonly PersonRepository _personRepository;
+
+        // if the program is taking over 30 seconds to run set this to something lower
+        private const int PeopleLimit = 10;
+
+
+        public CountryLogic()
+        {
+            _personRepository = new PersonRepository();
+        }
+
+        public void WritePeopleOrderedByCountry(string outputFileName)
+        {
+            if (System.IO.File.Exists(outputFileName))
+            {
+                System.IO.File.Delete(outputFileName);
+            }
+          
+            List<Country> countries = _personRepository.GetCountries();
+
+            foreach (Country country in countries)
+            {
+                List<Person> people = _personRepository.GetPeopleFromCountry(country.CountryID, PeopleLimit);
+
+                foreach (Person person in people)
+                {
+                    System.IO.File.AppendAllText(outputFileName, $"{country.CountryName} - {person.FirstName} {person.LastName}" + Environment.NewLine);
+                }
+
+                Console.WriteLine($"Written {people.Count} people who are in {country.CountryName}");
+            }
+        }
+
+    }
+}
