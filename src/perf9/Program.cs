d67aecda4b3112f068a8689b24fe69095244b2b3
Lachlan Barclay <lachlan.barclay@mercer.com>
2020-09-15 16:58:12 +1000

    added BenchmarkDotNet example

---
+++ src/perf9/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+using BenchmarkDotNet.Running;
+
+namespace perf9
+{
+    
+    
+    public class QuickTest
+    {
+        [Benchmark]
+        public void ForEach()
+        {
+            int i = 0;
+            var people = GetPeopleEnumerate();
+            foreach (var person in people)
+            {
+                if (++i % 100 == 0)
+                    Console.WriteLine(person.ToString());
+            }
+        }
+
+        [Benchmark]
+        public void ForLoop()
+        {
+            var people = GetPeopleEnumerate().ToArray();
+
+            for (int i=0;i<people.Count();i++)
+            {
+                if (i % 100 == 0)
+                    Console.WriteLine(people[i].ToString());
+            }
+        }
+
+        private IEnumerable<Person> GetPeopleEnumerate()
+        {
+            using (SqlConnection sc = new SqlConnection(_localDb))
+            {
+                sc.Open();
+
+                string sql = "SELECT top 1000 p.personid, p.firstname, p.lastname, c.countryname " +
+                             "FROM   person p inner join country c on p.countryid = c.countryid " +
+                             "order by c.countryName";
+
+                using (SqlCommand command = new SqlCommand(sql, sc))
+                {
+                    using (SqlDataReader sqlDataReader = command.ExecuteReader())
+                    {
+                        while (sqlDataReader.Read())
+                        {
+                            yield return new Person(sqlDataReader);
+                        }
+                    }
+                }
+            }
+        }
+
+        private string _localDb
+        {
+            get
+            {
+                var path = new FileInfo(System.IO.Path.Combine(Environment.CurrentDirectory, "..", "..", "..", "..", "..", "..", "..", "..", "..", "data"));
+                var datafile = path.FullName + "\\peopledb.mdf";
+
+                return "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;AttachDbFileName=" + datafile;
+            }
+        }
+
+    }
+
+    public class Person
+    {
+        public int PersonID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CountryID { get; set; }
+        public string CountryName { get; set; }
+
+        public Person(SqlDataReader sqlDataReader)
+        {
+            this.PersonID = sqlDataReader.GetInt32(0);
+            this.FirstName = sqlDataReader.GetString(1);
+            this.LastName = sqlDataReader.GetString(2);
+            this.CountryName = sqlDataReader.GetString(3);
+        }
+
+        public override string ToString()
+        {
+            return this.CountryName + " - " + this.FirstName + " " + this.LastName + Environment.NewLine;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<QuickTest>();
+        }
+    }
+}
